5ac248bf9b72a588b5a853782881e5d418455fe3
tigerdone <3196083633@qq.com>
2017-11-01 13:03:47 +0800

    add first time

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index),
+    # url(r'^article/(?P<article_id>[0-9]+)$',
+    #     views.article_page, name='article_page'),
+
+    url(r'^article/(?P<article_id>[0-9]+)$', views.article_page),
+]
+
+
